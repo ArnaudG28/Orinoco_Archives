5f6e1a53b4b2f8dd7122f9a52e05802da420f181
ArnaudG28 <85293544+ArnaudG28@users.noreply.github.com>
2021-10-18 17:26:32 +0200

    Version V.1.3 maquettage
    Version V.1.3 maquettage page panier et confirmation

---
+++ js/fonctions.js
@@ -0,0 +1,14 @@
+//Fonctions Globales
+
+function pageConfirmation() {
+  window.location.href = './confirmation.html';
+}
+
+function pagePanier() {
+  window.location.href = './panier.html';
+
+}
+
+function pageAccueil() {
+  window.location.href = './index.html';
+}
